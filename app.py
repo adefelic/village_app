f321f6fe245dc0abf02711335e28a4afdb188782
adamdefelice <ad.defelice@live.com>
2018-01-20 16:21:18 -0500

    what even

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 from flask import render_template
 import json
+from flask import request
 
 def _get_json(path):
     with open(path) as json_data:
@@ -24,10 +25,29 @@ def groups():
 
 @app.route("/help-me/", methods=['GET'])
 def help_me():
+    if (request.args) :
+        # load task, group, date, user
+        # new task {	
+        # "tasks": [
+		# {
+		#	"id": "" # bottom task id - "t_",  + 1 
+		# 	"description": "pick up my kid",
+		# 	"group": request.args.get("group")
+		# 	"created_by": request.args.get("user")
+		# 	"helper": "",
+		# 	"when": request.args.get("when") -> to unix time
+		# 	"expires": the above, + 2 hours
+		# },}
+        # 
+        # update tasks list in group obj
+        #
+        # update tasks list in user obj
+        #
+        # 
     tasks = _get_json('task.json')
     users = _get_json('users.json')
     groups = _get_json('groups.json')
-    return render_template('help-me.html',tasks=tasks,users=users,groups=groups)
+    return render_template('help-me.html', tasks=tasks,users=users,groups=groups)
 
 @app.route("/tasks/", methods=['GET'])
 def tasks():
