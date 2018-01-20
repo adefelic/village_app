e0c6f419f593c31e8854baa369b5b986b0d3ccf4
adamdefelice <ad.defelice@live.com>
2018-01-20 10:03:43 -0500

    added more placeholder stuff

---
+++ data.js
@@ -0,0 +1,44 @@
+var users = [
+	{
+		"id": "u_0",
+		"name": adam,
+		"groups": [
+			// lists groups this user belongs to
+			"g_0",
+			"g_3"
+		]
+		"village": [
+			// users this user has approved
+			"u_1"
+		]
+		"tasks": [
+			{
+				"id": "t_0",
+			}
+
+		]
+	}
+];
+
+var groups = [
+	{
+		"id": "g_0",
+		"name": "international school of boston",
+		"users": [ // lists users that belong to this group
+			"u_0"
+		]
+	}
+
+];
+
+var tasks = [
+	{
+		"id": "t_0",
+		"description": "pick up my kid!"
+		"group": "g_0"
+		"created_by": "u_0",
+		"helper": "u_1",
+		"when": 0987093845098345
+		"expires": 98723408803240823094
+	}
+];
