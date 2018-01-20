875c2e2a28152b5a0323d3da06c55ed040c346b3
Allison <allisonmariebrowne@gmail.com>
2018-01-20 13:13:14 -0500

    setup flask

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # village_app
+
+To get flask up and running
+
+pip install Flask
+FLASK_APP=app.py flask run
+* Running on http://localhost:5000/
+
+Navigate to port 5000
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/groups/", methods=['GET'])
+def groups():
+    return render_template('groups.html')
+
+@app.route("/help-me/", methods=['GET'])
+def help_me():
+    return render_template('help-me.html')
+
+@app.route("/tasks/", methods=['GET'])
+def tasks():
+    return render_template('tasks.html')
