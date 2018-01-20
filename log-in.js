bcbaa5d667f2bf4ad4f06c48fc9c45eecbd2fddd
Audrey Lee <audreylee@Audreys-MacBook-Pro.local>
2018-01-20 11:53:16 -0500

    log in

--- log-in.js
+++ log-in.js
@@ -1,4 +1,27 @@
-// code taken from https://www.w3schools.com/js/js_cookies.asp
+/* Code taken from https://www.w3schools.com/howto/howto_js_dropdown.asp
+When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// Code taken from https://www.w3schools.com/js/js_cookies.asp
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
