62c063f48703deed0fb4164d15b3cf374fc275df
Allison <allisonmariebrowne@gmail.com>
2018-01-20 14:38:58 -0500

    Add data to get requests keep other file for now

--- app.py
+++ app.py
@@ -3,18 +3,36 @@ app = Flask(__name__)
 from flask import render_template
 import json
 
+def _get_json(path):
+    with open(path) as json_data:
+        return json.loads(json_data.read())
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    tasks = _get_json('task.json')
+    users = _get_json('users.json')
+    groups = _get_json('groups.json')
+    return render_template('index.html', tasks=tasks, users=users, groups=groups,)
 
 @app.route("/groups/", methods=['GET'])
 def groups():
-    return render_template('groups.html')
+    tasks = _get_json('task.json')
+    users = _get_json('users.json')
+    groups = _get_json('groups.json')
+    return render_template('groups.html', tasks=tasks, users=users, groups=groups)
 
 @app.route("/help-me/", methods=['GET'])
 def help_me():
-    return render_template('help-me.html')
+    tasks = _get_json('task.json')
+    users = _get_json('users.json')
+    groups = _get_json('groups.json')
+    return render_template('help-me.html',tasks=tasks,users=users,groups=groups)
 
 @app.route("/tasks/", methods=['GET'])
 def tasks():
-    return render_template('tasks.html')
+    return render_template(
+        'tasks.html',
+        tasks=tasks,
+        users=users,
+        groups=groups,
+    )
